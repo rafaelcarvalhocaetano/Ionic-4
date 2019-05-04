e9dc618c6d2964fd179539ead3b30a48ed3ed51e
Rafael Carvalho Caetano <rapha.pse@outlook.com>
2019-05-04 18:04:23 -0300

    Create Project PWS Ionic 4

---
+++ pwa-tasks/src/app/shared/shared.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { IonicModule } from '@ionic/angular';
+
+@NgModule({
+  // imports: [CommonModule, FormsModule, IonicModule, ReactiveFormsModule],
+  exports: [CommonModule, FormsModule, IonicModule, ReactiveFormsModule]
+})
+export class SharedModule {}
