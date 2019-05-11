305f5fcb678e7e15d616fcad919a640e4d2f6eb6
Rafael Carvalho Caetano <rapha.pse@outlook.com>
2019-05-11 15:48:39 -0300

    Update file

--- pwa-tasks/src/app/auth/auth.module.ts
+++ pwa-tasks/src/app/auth/auth.module.ts
@@ -1,12 +1,5 @@
 import { NgModule } from '@angular/core';
-
 import { AuthRoutingModule } from './auth-routing.module';
-import { Routes } from '@angular/router';
-
-
-const routes: Routes = [
-  { path: '', loadChildren: './pages/login/login.module#LoginPageModule'}
-];
 
 @NgModule({
   declarations: [],
