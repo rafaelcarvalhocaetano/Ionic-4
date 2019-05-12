d749c14e959787f4d704539be7b4c57fdcc9c45d
Rafael Carvalho Caetano <rapha.pse@outlook.com>
2019-05-12 12:48:05 -0300

    Routing guard

--- pwa-tasks/src/app/app-routing.module.ts
+++ pwa-tasks/src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './core/guards/auth.guard';
 
 const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', loadChildren: './auth/auth.module#AuthModule' },
-  { path: 'task-list', loadChildren: './task/pages/task-list/task-list.module#TaskListPageModule' },
+  { path: 'task', loadChildren: './task/task.module#TaskModule', canLoad: [AuthGuard] },
 ];
 
 @NgModule({
---
+++ pwa-tasks/src/app/task/task.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+
+import { TaskRoutingModule } from './task-routing.module';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    TaskRoutingModule
+  ]
+})
+export class TaskModule { }
