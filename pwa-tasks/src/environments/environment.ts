ad853dfca641435e72f2da5ef55859473112a860
Rafael Carvalho Caetano <rapha.pse@outlook.com>
2019-05-18 13:28:21 -0300

    Page Create

--- pwa-tasks/src/environments/environment.ts
+++ pwa-tasks/src/environments/environment.ts
@@ -4,7 +4,15 @@
 
 export const environment = {
   production: false,
- 
+  firebase: {
+    apiKey: 'AIzaSyDJGph1CNrTndAcsaUweYC96bWWcwDOCRg',
+    authDomain: 'projeto-ionic4.firebaseapp.com',
+    databaseURL: 'https://projeto-ionic4.firebaseio.com',
+    projectId: 'projeto-ionic4',
+    storageBucket: 'projeto-ionic4.appspot.com',
+    messagingSenderId: '288692109361',
+    appId: '1:288692109361:web:8be31d7e165cb160'
+  }
   
 };
 
