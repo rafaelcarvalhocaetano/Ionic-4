40be6e970a7a82ec3a97e2d7258d9c298282175c
Rafael Carvalho Caetano <rapha.pse@outlook.com>
2019-05-18 17:41:55 -0300

    Finish project

--- pwa-tasks/src/app/app.component.ts
+++ pwa-tasks/src/app/app.component.ts
@@ -3,34 +3,35 @@ import { Component } from '@angular/core';
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { AuthService } from './core/services/auth.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html'
 })
 export class AppComponent {
-
-
-
   pages: { url: string, direction: string, icon: string, text: string }[];
+  user: string;
 
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    private statusBar: StatusBar,
+    private authService: AuthService
   ) {
     this.initializeApp();
   }
 
   initializeApp() {
-
-
     this.pages = [
       {url: '/task', direction: 'back', icon: 'checkmark', text: 'Tasks'},
       { url: '/task/create', direction: 'forward', icon: 'add', text: 'New Task'}
-    ]
-
-
+    ];
+    this.authService.authState$.subscribe(user => {
+      if (user && user !== null) {
+        (this.user = user.displayName)
+      }
+    });
     this.platform.ready().then(() => {
       this.statusBar.styleDefault();
       this.splashScreen.hide();
--- pwa-tasks/src/app/shared/shared.module.ts
+++ pwa-tasks/src/app/shared/shared.module.ts
@@ -2,9 +2,22 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
+import { MenuToggleComponent } from './components/menu-toggle/menu-toggle.component';
+import { LogoutButtonComponent } from './components/logout-button/logout-button.component';
 
 @NgModule({
+  declarations: [
+    MenuToggleComponent,
+    LogoutButtonComponent
+  ],
   imports: [IonicModule],
-  exports: [CommonModule, FormsModule, IonicModule, ReactiveFormsModule]
+  exports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    ReactiveFormsModule,
+    MenuToggleComponent,
+    LogoutButtonComponent
+  ]
 })
 export class SharedModule {}
